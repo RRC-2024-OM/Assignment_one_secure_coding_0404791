5e751c87fb1db2ca7f8da05f8ad82286c50892b1
OM J PATEL <opatel@academic.rrc.ca>
2025-03-10 17:13:17 -0500

    Replace os.system with subprocess.run to prevent OS command injection in send_email - OP

--- assignment1_code_sample (1).py
+++ assignment1_code_sample (1).py
@@ -1,4 +1,3 @@
-import os
 import pymysql
 from urllib.request import urlopen
 import subprocess
@@ -15,7 +14,7 @@ def get_user_input():
     return user_input
 
 def send_email(to, subject, body):
-    os.system(f'echo {body} | mail -s "{subject}" {to}')
+    subprocess.run(['mail', '-s', subject, to], input=body.encode())
 
 def get_data():
     url = 'http://insecure-api.com/get-data'
